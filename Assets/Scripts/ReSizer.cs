958f899e9f4327a0c3977dc3a60a6444da0b33b7
Asura <70947709+AsuraDark@users.noreply.github.com>
2025-08-31 21:01:55 +0900

    Task "Transformation" fix1

---
+++ Assets/Scripts/ReSizer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReSizer : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        Vector3 newScale = transform.localScale;
+        newScale *= _speed;
+
+        transform.localScale += newScale * Time.deltaTime;
+    }
+}
